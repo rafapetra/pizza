c14c1b06cf5944e848320bf1f1115726bafcde06
Rafael Petrachini <raffp@me.com>
2022-10-04 20:54:16 -0700

    Fix JS code for resubmission

--- js/script.js
+++ js/script.js
@@ -1,26 +1,20 @@
 // Business Logic for Pizza ---
 function Pizza(pizzaSize, pizzaToppings) {
-  this.toppings = [pizzaToppings];
+  this.toppings = [...pizzaToppings];
   this.size = pizzaSize;
 }
 
-Pizza.prototype.addPrice = function (price) {
-  this.price = 0;
-  if (this.size.includes("small")) {
-    this.price = 10 + this.toppings[0].length * 2;
-  } else if (this.size.includes("medium")) {
-    this.price = 15 + this.toppings[0].length * 2;
-  } else if (this.size.includes("large")) {
-    this.price = 20 + this.toppings[0].length * 2;
+Pizza.prototype.addPrice = function () {
+  if (this.size === "small") {
+    this.price = 10 + this.toppings.length * 2;
+  } else if (this.size === "medium") {
+    this.price = 15 + this.toppings.length * 2;
+  } else if (this.size === "large") {
+    this.price = 20 + this.toppings.length * 2;
   };
-
-  this.toppings.forEach(function(element, i, array){
-  });
 }; 
 
 // User Interface Logic -----
-
-let newOrder = new Pizza();
 
 function handleFormSubmission(event) {
   event.preventDefault();
@@ -38,14 +32,10 @@ function handleFormSubmission(event) {
   for (let i = 0; i < checkboxes.length; i++) {
     selectedToppings.push(checkboxes[i].value);
   }
-
-  newOrder.toppings = selectedToppings;
-  newOrder.size = inputtedSize;
+  let newOrder = new Pizza(inputtedSize, selectedToppings);
   newOrder.addPrice();
   document.querySelector("div#result").style.display = "block";
-
   document.querySelector("span#result").innerText = "$"+newOrder.price;
-  
 }
 
 window.addEventListener("load", function () {
